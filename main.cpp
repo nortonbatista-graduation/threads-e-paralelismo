b5e1ae3b7960f170f02b0b4679db45879ce2b7aa
Norton Batista Abdala <nortonbatistaa@gmail.com>
2021-04-11 22:23:09 -0300

    Create, show and multiply matrices

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <chrono>
+#include "lib/timer.h"
+#include "lib/matrix.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    srand(time(NULL));
+
+    auto start = now();
+    int width = 3;
+    int height = 2;
+
+    matrix_struct* matrix_a = generate_matrix(height, width);
+    matrix_struct* matrix_b = generate_matrix(width, height);
+    matrix_struct* multiplied_matrix = generate_matrix(height, width);
+
+    cout << "Matrix A" << endl;
+
+    print_matrix(matrix_a);
+
+    cout << "Matrix B" << endl;
+
+    print_matrix(matrix_b);
+
+    cout << "Multiplied Matrices" << endl;
+
+    multiplied_matrix = multiply_matrix(matrix_a, matrix_b);
+    print_matrix(multiplied_matrix);
+
+    auto finish = now();
+
+    double elapsed_time_s = convert_to_seconds(elapsed_time(start, finish));
+
+    cout << elapsed_time_s;
+
     return 0;
 }
